bef4062f3b798febe60a4b8e9a383eede4b36408
Thiago Allves <101152758+thiallves@users.noreply.github.com>
2025-12-06 17:06:28 -0300

    SCRIPT_IMPORTACAO_BASE_ORIGINAL

---
+++ tabela_original.sql
@@ -0,0 +1,27 @@
+create table tabela_original( 
+    timestamp varchar(50), 
+    age int, gender varchar(25), 
+    Purchase_Frequency Varchar(100),
+    Purchase_Categories Varchar(100), 
+    Personalized_Recommendation_Frequency Varchar(100), 
+    Browsing_Frequency Varchar(100),
+    Product_Search_Method Varchar(100), 
+    Search_Result_Exploration Varchar(100), 
+    Customer_Reviews_Importance Integer, 
+    Add_to_Cart_Browsing Varchar(100), 
+    Cart_Completion_Frequency Varchar(100), 
+    Cart_Abandonment_Factors Varchar(100), 
+    Saveforlater_Frequency Varchar(100), 
+    Review_Left Varchar(100), 
+    Review_Reliability Varchar(100), 
+    Review_Helpfulness Varchar(100), 
+    Personalized_Recommendation_Frequency_Score Integer, 
+    Recommendation_Helpfulness Varchar(100), 
+    Rating_Accuracy Integer, 
+    Shopping_Satisfaction Integer, 
+    Service_Appreciation Varchar(100), 
+    Improvement_Areas Varchar(100) 
+); 
+
+select * FROM tabela_original; 
+
